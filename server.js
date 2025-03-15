ac7055277b515a539793c51bd9b3932d6f5a8cd9
Nathan Barchie <sevenbridgesofsark@proton.me>
2025-03-15 12:48:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.use(
 
 //connect to MySQL DB
 const db = mysql.createConnection({
-  host: "t14librarydb.mysql.database.azure.com",
-  user: "T14Admin",
-  password: "AtticRaisin3380",
-  database: "librarysystem",
+  host: "",
+  user: "",
+  password: "",
+  database: "",
 });
 
 db.connect();
