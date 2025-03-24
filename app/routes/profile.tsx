8ee259e146054d152b2cc1983db53d3d1bed3786
brix-diana <brixdiana312@gmail.com>
2025-03-23 21:21:11 -0500

    created profile css

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from "react-router";
 import { type AuthData } from "~/services/api";
+import "./profile.css";
 
 export default function ProfilePage() {
   const {
@@ -13,7 +14,7 @@ export default function ProfilePage() {
   } = useOutletContext<AuthData>();
   return (
     <div>
-      <h2>Welcome to profile page #{memberID}</h2>
+    <h2>Welcome to profile page {firstName} {lastName}! #{memberID}</h2>
       <h2>{groupID}</h2>
       <h2>First Name: {firstName}</h2>
       <h2>Middle Name: {middleName}</h2>
